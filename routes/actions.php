01b4d0b6b33954e60a21bb8405f9678a6f8b6c7e
edalzell <erin@thedalzells.org>
2021-03-09 16:16:19 -0800

    simplify

--- routes/actions.php
+++ routes/actions.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use TransformStudios\Drafts\Http\Controllers\ViewDraftController;
 
-Route::name('drafts.')->group(function () {
-    Route::get('{id}', [DraftController::class, '__invoke'])->name('show');
-});
+Route::get('{id}', [ViewDraftController::class, '__invoke'])->name('drafts.show');
