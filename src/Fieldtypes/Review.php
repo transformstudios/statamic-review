7f97d20306eb497dec64137b0e9efd576eae5599
Erin Dalzell <erin@silentz.co>
2022-09-08 10:08:28 -0700

    Use proper site url (#16)

--- src/Fieldtypes/Review.php
+++ src/Fieldtypes/Review.php
@@ -2,7 +2,9 @@
 
 namespace TransformStudios\Review\Fieldtypes;
 
+use Illuminate\Support\Arr;
 use Statamic\Entries\Collection;
+use Statamic\Entries\Entry;
 use Statamic\Facades\Site;
 use Statamic\Fields\Fieldtype;
 
@@ -21,7 +23,7 @@ class Review extends Fieldtype
 
     public function icon()
     {
-        return file_get_contents(__DIR__ . '/../../resources/svg/review-icon.svg');
+        return file_get_contents(__DIR__.'/../../resources/svg/review-icon.svg');
     }
 
     public function preload()
@@ -37,7 +39,7 @@ class Review extends Fieldtype
         }
 
         return [
-            'site_url' => Site::default()->url(),
+            'site_url' => $this->makeUrl($entry),
             'has_revision' => $entry->hasWorkingCopy(),
         ];
     }
@@ -45,7 +47,7 @@ class Review extends Fieldtype
     /**
      * Pre-process the data before it gets sent to the publish page.
      *
-     * @param mixed $data
+     * @param  mixed  $data
      * @return array|mixed
      */
     public function preProcess($data)
@@ -56,11 +58,19 @@ class Review extends Fieldtype
     /**
      * Process the data before it gets saved.
      *
-     * @param mixed $data
+     * @param  mixed  $data
      * @return array|mixed
      */
     public function process($data)
     {
         return $data;
+    }
+
+    private function makeUrl(Entry $entry): string
+    {
+        $parsed = parse_url(Site::get($entry->locale())->url());
+        $port = Arr::has($parsed, 'port') ? ":{$parsed['port']}" : '';
+
+        return $parsed['scheme'].'://'.$parsed['host'].$port;
     }
 }
