546b261f7e66eb4fffa5c685ed6802690d10c4ce
edalzell <erin@silentz.co>
2022-09-08 10:02:32 -0700

    support static caching (nocache tag)

--- routes/actions.php
+++ routes/actions.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Statamic\Http\Middleware\AddViewPaths;
+use Statamic\StaticCaching\Middleware\Cache;
 use TransformStudios\Review\Http\Controllers\ReviewController;
 use TransformStudios\Review\Http\Middleware\SetSite;
 
@@ -9,4 +10,5 @@ Route::get('{id}', [ReviewController::class, '__invoke'])
     ->middleware([
         SetSite::class,
         AddViewPaths::class,
+        Cache::class,
     ])->name('review.show');
