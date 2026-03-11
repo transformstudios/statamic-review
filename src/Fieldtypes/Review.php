aa9ceec29fda838917afd08b4c24f7da499bd58d
Erin Dalzell <erin@silentz.co>
2026-03-11 14:42:25 -0700

    Remove Open Review action (#67)

--- src/Fieldtypes/Review.php
+++ src/Fieldtypes/Review.php
@@ -3,8 +3,11 @@
 namespace TransformStudios\Review\Fieldtypes;
 
 use Statamic\Entries\Collection;
+use Statamic\Entries\Entry;
+use Statamic\Facades\Token as TokenFacade;
 use Statamic\Fields\Fieldtype;
-use TransformStudios\Review\Support\URL;
+use Statamic\Tokens\Token;
+use TransformStudios\Review\TokenHandler;
 
 class Review extends Fieldtype
 {
@@ -33,6 +36,19 @@ class Review extends Fieldtype
             return [];
         }
 
-        return ['site_url' => review_url($entry)];
+        return ['site_url' => $this->url($entry)];
+    }
+
+    private function url(Entry $entry): string
+    {
+        /** @var \Statamic\Tokens\Token */
+        if (! $token = TokenFacade::find($entry->id())) {
+            $token = tap(
+                TokenFacade::make(token: $entry->id(), handler: TokenHandler::class),
+                fn (Token $token) => $token->expireAt(now()->addMonths(6))->save()
+            );
+        }
+
+        return $entry->absoluteUrl().'?token='.$token->token();
     }
 }
